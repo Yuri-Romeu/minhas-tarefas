0d13f6018ff5af7d6154a6b9d5bfc5d2082a428b
Yuri Romeu <yuriromeu2007@gmail.com>
2025-08-06 08:34:31 -0300

    correção de bug e projeto finalizado

---
+++ src/@types/custom.d.ts
@@ -0,0 +1,7 @@
+declare module 'aria-query';
+declare module 'hoist-non-react-statics';
+declare module 'react-aria-components';
+declare module 'react-aria-components/ssr';
+declare module 'react-aria-components/ssr/server';
+declare module 'react-aria-components/ssr/client';
+declare module 'react-aria-components/ssr/server/ssr';
